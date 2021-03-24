b1ce2cf19c9dfa9fe7e3871f1f906ca3b8118d19
DigitalJS24 <68379842+DigitalJS24@users.noreply.github.com>
2021-03-24 18:34:31 +0200

    validate and notify

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/style.css';
+import { validate } from './helpers/validate';
 
 import UI from './config/ui.config';
+import { showInputError, removeInputError } from './views/form';
+import { login } from './services/auth.service';
+import { notify } from './views/notification';
 
 const { form, inputEmail, inputPassword } = UI;
+const inputs = [inputEmail, inputPassword];
 
 //events
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    inputs.forEach(el => {
+        removeInputError(el);
+    });
     onSubmit();
 })
 
+inputs.forEach(el => {
+    el.addEventListener('focus', () => removeInputError(el));
+})
 
 //handlers
 
-function onSubmit() {
+async function onSubmit() {
+    const isValideForm = inputs.every((el) => {
+        const isValideInput = validate(el);
+        if (!isValideInput) { showInputError(el) }
+        return isValideInput;
+    })
+    if (!isValideForm) return;
+    try {
+        await login(inputEmail.value, inputPassword.value);
+        form.reset();
+        notify({ msg: 'Login success', className: 'alert-success' })
 
+    } catch (err) {
+        notify({ msg: 'Login failed', className: 'alert-danger', timeout: 3000 });
+    }
 }
--- src/js/config/ui.config.js
+++ src/js/config/ui.config.js
@@ -1,7 +1,7 @@
 const UI = {
     form: document.forms['loginForm'],
-    inputEmail: document.getElementById('email');
-    inputPassword: document.getElementById('password');
+    inputEmail: document.getElementById('email'),
+    inputPassword: document.getElementById('password'),
 };
 
 export default UI;
